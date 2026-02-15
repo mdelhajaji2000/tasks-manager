5b02cc1deac0c67e63e44f9ed550459684a2c2cd
mdelhajaji2000 <149912377+mdelhajaji2000@users.noreply.github.com>
2026-02-15 21:43:07 +0000

    Initial solution structure for Task Manager app
    Set up .NET Framework 4.7.2 solution with Domain, BLL, DAL, and WinForms Presentation Layer projects. Added placeholder classes, project files, resource/configuration files, and solution management files. No business logic implemented yet; foundation for multi-layered architecture is established.

---
+++ task manager PL/task manager Bl/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("task manager Bl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task manager Bl")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1c51f3d-1c77-4c2e-89c9-a725fb5608db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
