d08f6b2bd17d09944613f2d41667021930fd1d55
mdelhajaji2000 <149912377+mdelhajaji2000@users.noreply.github.com>
2026-02-17 01:45:06 +0000

    Add initial database helper classes and project setup
    Added SqlDb class with ExecuteNonQuery method for SQL command execution. Introduced placeholder classes for transactions and parameter handling. Included custom DbAccessException. Set target framework to .NET 4.7.2 and added build cache files. These changes establish the foundation for a database helper library.

---
+++ ADO_Helper/Db/SqlDb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Helper.Db
+{
+    public class SqlDb
+    {
+        private readonly string _connectionString;
+
+        public SqlDb(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException (
+                    "Connection string cannot be null or empty",
+                    nameof(connectionString)
+                    );
+
+            _connectionString = connectionString;
+        }
+
+        public int ExecuteNonQuery(string Query, CommandType commandType = CommandType.Text,
+            int timeout = 30, params SqlParameter[] parameters)
+        {
+            if (string.IsNullOrWhiteSpace(Query))
+                throw new ArgumentException(
+                    "Query cannot be empty !",
+                    nameof(Query)
+                    );
+
+            int AffectedRows = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(Query, connection))
+                    {
+                        command.CommandType = commandType;
+                        command.CommandTimeout = timeout;
+
+                        if (parameters != null && parameters.Length > 0)
+                            command.Parameters.AddRange(parameters);
+
+                        AffectedRows = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); // Exceptions Will be handeled in the right way later on
+            }
+            return AffectedRows;
+        }
+    }
+}
---
+++ ADO_Helper/Exceptions/DbAccessExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Helper.Exceptions
+{
+    internal class DbAccessException : Exception
+    {
+        public DbAccessException(string message, Exception inner) : base(message, inner) { }
+    }
+}
